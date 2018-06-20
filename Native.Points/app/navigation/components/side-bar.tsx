ca60d60eabfe9cf3214c962b2196f5df7d481a2e
Joe Badaczewski <badaczewskij@live.com>
2018-06-20 15:16:53 -0400

    adding dynamic nav items to side bar

--- Native.Points/app/navigation/components/side-bar.tsx
+++ Native.Points/app/navigation/components/side-bar.tsx
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import { Container, Content, Text, List, ListItem } from 'native-base';
+import { NavigationItemDto } from '@points/shared';
 
 import { NavigationActions } from 'react-navigation';
+import { navItems } from '../../store/selectors'
+import store from '../../store';
 
-// TODO populate this with selector
-const routes = ['Home', 'AchievementList', 'Profile'];
+export interface ISideBarState {
+    routes: string[];
+}
 
-export default class SideBar extends Component<{ navigation: any }> {
+export default class SideBar extends Component<{ navigation: any }, ISideBarState> {
+
+    routes: string[] = [];
+
+    state: ISideBarState = {
+        routes: []
+    }
+
+    constructor(props: { navigation: any }) {
+        super(props);
+
+        store.subscribe(navItems((navItems: NavigationItemDto[]) =>
+            this.setState({
+                routes: navItems.map(item => item.name)
+            })));
+    }
+
     public render(): JSX.Element {
         return (
             <Container>
                 <Content>
                     <List
-                        dataArray={routes}
+                        dataArray={this.state.routes}
                         renderRow={(data: string) => {
                             return (
                                 <ListItem
