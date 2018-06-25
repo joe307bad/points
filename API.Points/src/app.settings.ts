4dc8dcf1c8f1bbdd7679b247eae8da319ac65aa6
Joe Badaczewski <badaczewskij@live.com>
2018-06-25 16:38:39 -0400

    updating DB url

--- API.Points/src/app.settings.ts
+++ API.Points/src/app.settings.ts
@@ -2,7 +2,7 @@ import { diskStorage } from 'multer';
 import { extname } from 'path';
 
 // TODO replace these values when bundling for deployment
-export const dbUrl = 'mongodb://localhost:27017/points'; // mongodb://mongodb:27017/points;
+export const dbUrl = 'mongodb://mongo:27017/points'; // mongodb://mongodb:27017/points;
 export const uploadDir = './public/uploads'; // ./dist/public/uploads
 export const secret = '8QnwdhUqb7TgebAwTwpvmBKdFgTE3bFNcDUL3DgTuFDG0';
 export const ApiIntentHeader = 'X-Api-Intent';
