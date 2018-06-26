a45f86b00417052844b4db3ea23280b751585279
Joe Badaczewski <badaczewskij@live.com>
2018-06-26 16:39:41 -0400

    implementing acceptable modal for uploading photo

--- Native.Points/app/achievement/components/list.tsx
+++ Native.Points/app/achievement/components/list.tsx
@@ -81,46 +81,47 @@ class AchievementPreview extends Component<{
 }> {
 
     public render() {
-        return (<Modal
-            style={{ height: 'auto', padding: 10, backgroundColor: 'transparent' }}
-            easing={Easing.elastic(0)}
-            position={'bottom'}
-            ref='achievementPreviewModal'>
-            <Card style={{ flex: 0 }}>
-                <CardItem header bordered style={{ paddingTop: 0, paddingBottom: 0 }}>
-                    <Left style={{ paddingLeft: 0, paddingRight: 0, paddingTop: 10, paddingBottom: 10 }}>
-                        <Thumbnail
-                            style={{ marginRight: 5 }}
-                            source={{
-                                // TODO store URL somewhere
-                                uri: this.props.selectedAchievement.photo
-                                    ? 'https://p.jbad.io/uploads/' + this.props.selectedAchievement.photo
-                                    : 'https://www.iconsdb.com/icons/preview/gray/circle-xxl.png'
-                            }} size={5} />
-                        <Text>{this.props.selectedAchievement.name}</Text>
-                    </Left>
-                    <PointsContainer achievement={this.props.selectedAchievement} />
-                </CardItem>
-                <CardItem>
-                    <Body>
-                        <Text>{this.props.selectedAchievement.description}</Text>
-                        <Button
-                            onPress={() => this.props.checkin({
-                                achievementId: this.props.selectedAchievement.achievementId,
-                                achievementName: this.props.selectedAchievement.name,
-                                userId: this.props.currentUser.userId,
-                                userName: this.props.currentUser.userName
-                            } as IUserCheckin)}
-                            style={{ marginTop: 15 }} full>
-                            <Icon type='Entypo' name='check' />
-                            <Text style={{
-                                color: 'white'
-                            }}>{'Check In'.toUpperCase()}</Text>
-                        </Button>
-                    </Body>
-                </CardItem>
-            </Card>
-        </Modal>
+        return (
+            <Modal
+                style={{ height: 'auto', padding: 10, backgroundColor: 'transparent' }}
+                easing={Easing.elastic(0)}
+                position={'bottom'}
+                ref='achievementPreviewModal'>
+                <Card style={{ flex: 0 }}>
+                    <CardItem header bordered style={{ paddingTop: 0, paddingBottom: 0 }}>
+                        <Left style={{ paddingLeft: 0, paddingRight: 0, paddingTop: 10, paddingBottom: 10 }}>
+                            <Thumbnail
+                                style={{ marginRight: 5 }}
+                                source={{
+                                    // TODO store URL somewhere
+                                    uri: this.props.selectedAchievement.photo
+                                        ? 'https://p.jbad.io/uploads/' + this.props.selectedAchievement.photo
+                                        : 'https://www.iconsdb.com/icons/preview/gray/circle-xxl.png'
+                                }} size={5} />
+                            <Text>{this.props.selectedAchievement.name}</Text>
+                        </Left>
+                        <PointsContainer achievement={this.props.selectedAchievement} />
+                    </CardItem>
+                    <CardItem>
+                        <Body>
+                            <Text>{this.props.selectedAchievement.description}</Text>
+                            <Button
+                                onPress={() => this.props.checkin({
+                                    achievementId: this.props.selectedAchievement.achievementId,
+                                    achievementName: this.props.selectedAchievement.name,
+                                    userId: this.props.currentUser.userId,
+                                    userName: this.props.currentUser.userName
+                                } as IUserCheckin)}
+                                style={{ marginTop: 15 }} full>
+                                <Icon type='Entypo' name='check' />
+                                <Text style={{
+                                    color: 'white'
+                                }}>{'Check In'.toUpperCase()}</Text>
+                            </Button>
+                        </Body>
+                    </CardItem>
+                </Card>
+            </Modal>
         );
     }
 }
--- Native.Points/app/upload/components/index.tsx
+++ Native.Points/app/upload/components/index.tsx
@@ -1,19 +1,43 @@
 import ImagePicker from 'react-native-image-picker';
 import React, { Component } from 'react';
-import { Container } from 'native-base';
+import { Container, Card, CardItem, Left, Body, Text, Button, Icon, View } from 'native-base';
+import Modal from 'react-native-modalbox';
+import { Easing, Image } from 'react-native';
 
 import { IBaseProps } from '../../navigation/components';
 import { Toolbar } from '../../shared/components/header';
+import { ScrollView } from 'react-native';
 
-export class Upload extends Component<IBaseProps> {
+interface ISelectedPhoto {
+    location: string;
+    base64: string;
+    height: number;
+    width: number;
+}
 
-    options = {
+interface IUploadState {
+    selectedPhoto: ISelectedPhoto;
+}
+
+export class Upload extends Component<IBaseProps, IUploadState> {
+
+    private uploadPreview?: Modal;
+    public cameraOptions = {
         title: 'Upload a photo',
         storageOptions: {
             skipBackup: true,
             path: 'images'
         }
     };
+
+    public state = {
+        selectedPhoto: {} as ISelectedPhoto
+    };
+
+    public componentDidMount() {
+        // @ts-ignore
+        this.uploadPreview = this.refs.uploadPreview.refs.uploadPreviewModal;
+    }
 
     public render(): JSX.Element {
 
@@ -23,7 +47,7 @@ export class Upload extends Component<IBaseProps> {
                     {...this.props}
                     camera
                     cameraHandler={() => {
-                        ImagePicker.showImagePicker(this.options, (response) => {
+                        ImagePicker.showImagePicker(this.cameraOptions, (response) => {
                             console.log('Response = ', response);
 
                             if (response.didCancel) {
@@ -41,11 +65,80 @@ export class Upload extends Component<IBaseProps> {
                                 // You can also display the image using data:
                                 // let source = { uri: 'data:image/jpeg;base64,' + response.data };
 
-                                debugger;
+                                // debugger;
+                                this.setState({
+                                    selectedPhoto: {
+                                        base64: response.data,
+                                        location: response.uri,
+                                        height: response.height,
+                                        width: response.width
+                                    }
+                                })
+                                this.uploadPreview!.open();
                             }
                         });
                     }} />
+                <UploadPreview photo={this.state.selectedPhoto} ref='uploadPreview' />
             </Container>
+        );
+    }
+}
+export class UploadPreview extends Component<{ photo: ISelectedPhoto }> {
+
+    public state = {
+        imagePreviewWidth: 0,
+        scrollViewHeight: 0
+    }
+
+    render(): JSX.Element {
+        return (
+            <Modal
+                style={{ height: 'auto', maxHeight: '100%', padding: 10, backgroundColor: 'transparent' }}
+                easing={Easing.elastic(0)}
+                coverScreen={true}
+                swipeToClose={false}
+                ref='uploadPreviewModal'>
+                <ScrollView style={{ height: 'auto' }}>
+                    <Card style={{ flex: 0 }}>
+                        <CardItem>
+                            <Body style={{ width: '100%' }}>
+                                <View
+                                    onLayout={(event) => {
+                                        const { height, width } = event.nativeEvent.layout;
+                                        this.setState({
+                                            imagePreviewWidth: width
+                                        })
+                                    }}
+                                    style={{
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        width: '100%',
+                                    }}>
+                                    <Image
+                                        resizeMode="contain"
+                                        style={{
+                                            height: this.props.photo.height,
+                                            width: this.state.imagePreviewWidth
+                                        }}
+                                        source={{ uri: this.props.photo.location }}
+                                    />
+                                </View>
+                                <Button
+                                    style={{ marginTop: 15 }} full>
+                                    <Icon type='Entypo' name='upload' />
+                                    <Text>Upload</Text>
+                                </Button>
+                                <Button
+                                    onPress={() => (this.refs.uploadPreviewModal as any).close()}
+                                    style={{ marginTop: 15 }} full danger>
+                                    <Icon type='Entypo' name='circle-with-cross' />
+                                    <Text>Cancel</Text>
+                                </Button>
+                            </Body>
+                        </CardItem>
+                    </Card>
+                </ScrollView>
+            </Modal>
         );
     }
 }
