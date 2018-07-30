f2763b27f3107ea49926244185aad0a4e5e54de2
Joe <badaczewskij@live.com>
2018-07-30 19:56:12 -0400

    enabling android native integration

--- Native.Points/index.js
+++ Native.Points/index.js
@@ -6,7 +6,7 @@ Sentry.config('https://85099796d541450596ddf73f69808c95@sentry.io/1247865', {
     deactivateStacktraceMerging: true,
     logLevel: SentryLog.Verbose,
     // currently sentry is not reporting errors on android using the native module
-    disableNativeIntegration: Platform.OS === 'android',
+    // disableNativeIntegration: Platform.OS === 'android',
 }).install();
 
 AppRegistry.registerComponent('points', () => App);
