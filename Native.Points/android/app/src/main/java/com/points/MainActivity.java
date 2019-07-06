8b0d4406bfdad799547bff08131556125099159f
Joe <badaczewskij@live.com>
2019-07-06 16:18:15 -0400

    getting react-native-photo-view and the image gallery to work

--- Native.Points/android/app/src/main/java/com/points/MainActivity.java
+++ Native.Points/android/app/src/main/java/com/points/MainActivity.java
@@ -1,5 +1,4 @@
 package com.points;
-
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
--- Native.Points/android/app/src/main/java/com/points/MainApplication.java
+++ Native.Points/android/app/src/main/java/com/points/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnative.photoview.PhotoViewPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,9 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
+            new MainReactPackage(),
             new ReactNativeConfigPackage(),
-            new RNGestureHandlerPackage()
+            new RNGestureHandlerPackage(),
+            new PhotoViewPackage()
       );
     }
 
