013cacd6d4d93bcefaee39a569f7e78c0c989812
joe307bad <joe@ybinteractive.com>
2019-07-07 16:47:30 -0400

    updating ios package and adding icons

--- Native.Points/android/app/src/main/java/com/points/MainApplication.java
+++ Native.Points/android/app/src/main/java/com/points/MainApplication.java
@@ -3,6 +3,9 @@ package com.points;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import io.sentry.RNSentryPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +29,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNSentryPackage(),
+            new ImagePickerPackage(),
             new ReactNativeConfigPackage(),
             new RNGestureHandlerPackage(),
             new PhotoViewPackage()
