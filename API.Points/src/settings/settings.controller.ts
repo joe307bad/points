eded2a4bdef6c7e230d2bea83b28e0abc283d861
Joe <badaczewskij@live.com>
2019-07-21 18:00:20 -0400

    filtering the correct settings based on role

--- API.Points/src/settings/settings.controller.ts
+++ API.Points/src/settings/settings.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, UseGuards, Get } from '@nestjs/common';
+import { Controller, UseGuards, Get, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import * as ac from 'accesscontrol';
 import { SettingsDto, ISettingsService } from '@points/shared';
 
-import { PermissionGaurd, ApiAction, ApiPermission, HasPermission } from '../core/acl';
+import { PermissionGaurd, ApiAction, ApiPermission, HasPermission, decodeToken } from '../core/acl';
 import { SettingsService } from './settings.service';
 
 const resource = 'settings';
@@ -11,11 +12,14 @@ export const to = (action: ApiAction) => new ApiPermission(action, resource);
 @Controller(resource)
 @UseGuards(AuthGuard('jwt'), PermissionGaurd)
 export class SettingsController implements ISettingsService {
-    constructor(private readonly settings: SettingsService) { }
+    constructor(private readonly settings: SettingsService,
+        private readonly access: ac.AccessControl) { }
 
     @Get()
     @HasPermission(to('read'))
-    async get(): Promise<SettingsDto> {
-        return await this.settings.get();
+    async get(@Req() req): Promise<SettingsDto> {
+        const user = decodeToken(req);
+        const permission = this.access.can(user.roles).readAny('settings');
+        return await this.settings.get().then(settings => permission.filter(settings));
     }
 }
--- API.Points/src/settings/settings.module.ts
+++ API.Points/src/settings/settings.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { SettingsController } from './settings.controller';
 import { SettingsService } from './settings.service';
+import { AcProvider } from '../core/acl';
 
 @Module({
     imports: [],
     controllers: [SettingsController],
-    providers: [SettingsService],
+    providers: [AcProvider, SettingsService],
 })
 export class SettingsModule {
 }
